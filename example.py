446bdb83c608d2d9866090d6ab38c0107017c5c5
Scott Pustay <muskox@users.noreply.github.com>
2017-01-16 11:35:00 -0900

    Setting reference to 1

--- example.py
+++ example.py
@@ -13,7 +13,7 @@ hx = HX711(dout=5, pd_sck=6)
 # Call get_weight before and after putting 1000g weight on your sensor.
 # Divide difference with grams (1000g) and use it as refference unit.
 
-hx.setReferenceUnit(21)
+hx.setReferenceUnit(1)
 
 hx.reset()
 hx.tare()
